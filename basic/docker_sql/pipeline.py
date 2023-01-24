2f7aa39a9f4ba7bf3c9e2d283e9a2cb5622e5382
dickymuhr <dicky.m.r@mail.ugm.ac.id>
2023-01-24 23:38:36 +0700

    add python program

---
+++ basic/docker_sql/pipeline.py
@@ -0,0 +1,11 @@
+import sys
+
+import pandas
+
+print(sys.argv) # mencetak semua cli argument
+
+day = sys.argv[1] # command line argument
+
+# some buzzword things that pemerintah ask us to do
+
+print(f"Job Finished in {day} day!")
