d72175fe8168f00d703a0aba7a1b5c9d4f676f3e
dickymuhr <dicky.m.r@mail.ugm.ac.id>
2023-01-26 22:36:51 +0700

    integrate zones table

--- basic/docker_sql/ingest_data.py
+++ basic/docker_sql/ingest_data.py
@@ -15,14 +15,22 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
+    table_lookup_name = params.table_lookup_name
+    url_lookup = params.url_lookup
 
     if url.endswith('.csv.gz'):
         csv_name = 'output.csv.gz'
     else:
         csv_name = 'output.csv'
+    if url_lookup.endswith('.csv.gz'):
+        csv_lookup_name = 'output_lookup.csv.gz'
+    else:
+        csv_lookup_name = 'output_lookup.csv'
 
     # Download the csv
     os.system(f"wget {url} -O {csv_name}")
+    os.system(f"wget {url_lookup} -O {csv_lookup_name}")
+
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     engine.connect()
@@ -34,7 +42,7 @@ def main(params):
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-
+    print("Ingesting NY Taxi Table ..")
     df.head(0).to_sql(name=table_name,con=engine,if_exists='replace')
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
@@ -50,6 +58,9 @@ def main(params):
         t_end = time()
         print(f"Inserted.. took {t_end-t_start} second")
 
+    print("Ingesting Zones Lookup Table ..")
+    df_zones = pd.read_csv(csv_lookup_name)
+    df_zones.to_sql(name=table_lookup_name, con=engine, if_exists='replace')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
@@ -64,6 +75,8 @@ if __name__ == '__main__':
     parser.add_argument('--db', help="db name for pg")
     parser.add_argument('--table_name', help="name of the table where will write the result to")
     parser.add_argument('--url', help="url to the csv file")
+    parser.add_argument('--table_lookup_name', help="name of the lookup table where will write the result to")
+    parser.add_argument('--url_lookup', help="url to the csv lookup file")
 
     args = parser.parse_args()
     main(args)
